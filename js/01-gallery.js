598828e562eddb857cefcb00ba8e62376d1b0696
KonstantinBYC <globalmacintosh@gmail.com>
2023-07-09 15:27:22 +0300

    Edit 09-07-23

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryCont = document.querySelector(".gallery");
+
+const markUp = createGallItemsMarkup(galleryItems);
+
+function createGallItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+            <a class="gallery__link" href = "${original}" >
+            < img class="gallery__image"
+        src = "${preview}"
+        alt = "${description}"
+        data-source = "${original}" /> </a> </li>`;
+    })
+    .join("");
+}
+galleryCont.insertAdjacentElement("beforeend", markUp);
+galleryCont.addEventListener("click", onClick);
+
+function onClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "BUTTON") {
+    return;
+  }
+  const source = e.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `<img src="${source}" width="800" height="600">`
+  );
+
+  instance.show();
+}
+
+document.addEventListener("keydown", (keyPressed) => {
+  if (keyPressed.key === "Escape") {
+    console.log("event listener added");
+    instance.close();
+  }
+});
+
+window.removeEventListener("keydown", onGalleryEsc);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const listElements = document.querySelector(".gallery");
+
+const markUp = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__a">
+            <a class="gallery__link" href = "${original}" >
+            < img class="gallery__image"
+        src = "${preview}"
+        alt = "${description}" width="800" height="600"/>
+        </a> </li>`;
+  })
+  .join(``);
+
+let lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+listElements.insertAdjacentHTML(`afterbegin`, markUp);
+listElements.addEventListener("click", onClick);
+
+function onClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "BUTTON") {
+    return;
+  }
+
+  const source = e.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src="${source}" width="800" height="600">`
+  );
+  instance.show();
+}
