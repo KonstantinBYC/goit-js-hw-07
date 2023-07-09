f518a49d70c67e80d574a4097103db5812ec0cf3
KonstantinBYC <globalmacintosh@gmail.com>
2023-07-10 00:11:16 +0300

    Galleries works

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,6 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryCont = document.querySelector(".gallery");
-
 const markUp = createGallItemsMarkup(galleryItems);
 
 function createGallItemsMarkup(items) {
@@ -10,35 +9,46 @@ function createGallItemsMarkup(items) {
     .map(({ preview, original, description }) => {
       return `<li class="gallery__item">
             <a class="gallery__link" href = "${original}" >
-            < img class="gallery__image"
-        src = "${preview}"
-        alt = "${description}"
+            <img class="gallery__image"
+        src="${preview}"
+        alt="${description}"
         data-source = "${original}" /> </a> </li>`;
     })
     .join("");
 }
-galleryCont.insertAdjacentElement("beforeend", markUp);
+
+galleryCont.insertAdjacentHTML("beforeend", markUp);
 galleryCont.addEventListener("click", onClick);
 
 function onClick(e) {
   e.preventDefault();
-  if (e.target.nodeName !== "BUTTON") {
-    return;
-  }
+  if (e.target.classList.contains("gallery")) return;
   const source = e.target.dataset.source;
-
   const instance = basicLightbox.create(
     `<img src="${source}" width="800" height="600">`
   );
-
   instance.show();
 }
 
-document.addEventListener("keydown", (keyPressed) => {
-  if (keyPressed.key === "Escape") {
-    console.log("event listener added");
-    instance.close();
-  }
-});
+// document.addEventListener("keydown", (keyPressed) => {
+//   if (keyPressed.key === "Escape") {
+//     console.log("event listener added");
+//     instance.close();
+//   }
+// });
 
-window.removeEventListener("keydown", onGalleryEsc);
+// const onShow = document.addEventListener("keydown", (keyPressed) => {
+//   if (keyPressed.key === "Escape") });
+//     const onClose = document.removeEventListener("keydown", (keyPressed) => {
+//       if (keyPressed.key === "Escape") });
+// function onClick(e) {
+//   e.preventDefault();
+//   if (e.target.nodeName !== "BUTTON") {
+//     return;
+//   }
+//   const source = e.target.dataset.source;
+
+//   const instance = basicLightbox.create(
+//     `<img src="${source}" width="800" height="600">`
+//   );
+//   instance.show(
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,35 +2,24 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const listElements = document.querySelector(".gallery");
+const markUp = createGallItemsMarkup(galleryItems);
 
-const markUp = galleryItems
-  .map(({ preview, original, description }) => {
-    return `<li class="gallery__a">
-            <a class="gallery__link" href = "${original}" >
-            < img class="gallery__image"
-        src = "${preview}"
-        alt = "${description}" width="800" height="600"/>
+function createGallItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img class="gallery__image"
+        src="${preview}"
+        alt="${description}" width="800" height="600"/>
         </a> </li>`;
-  })
-  .join(``);
+    })
+    .join(``);
+}
 
-let lightbox = new SimpleLightbox(".gallery__item", {
+listElements.insertAdjacentHTML(`afterbegin`, markUp);
+
+let lightbox = new SimpleLightbox(".gallery__link", {
   captionsData: "alt",
   captionDelay: 250,
 });
-
-listElements.insertAdjacentHTML(`afterbegin`, markUp);
-listElements.addEventListener("click", onClick);
-
-function onClick(e) {
-  e.preventDefault();
-  if (e.target.nodeName !== "BUTTON") {
-    return;
-  }
-
-  const source = e.target.dataset.source;
-  const instance = basicLightbox.create(
-    `<img src="${source}" width="800" height="600">`
-  );
-  instance.show();
-}
